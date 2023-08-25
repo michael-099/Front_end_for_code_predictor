f40cb0d2f36d7f0591c360bd561e58f39d41d91b
unknown <michaelshimeles099@gmail.com>
2023-08-25 17:11:24 +0300

    logo

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -2,7 +2,10 @@ import "./head.css";
 function Head() {
   return (
     <div className="top">
-      <p className="top-log"> Code whiz</p>
+      <div className="logo_name"> 
+        <img src="image.png" width="60px" height="60px" />
+        <p className="top-log"> Code whiz</p>
+      </div>
     </div>
   );
 }
