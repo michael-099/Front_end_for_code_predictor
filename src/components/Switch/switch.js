e983e2519c553b3f0b2315dc1260a3c2e25354a0
unknown <michaelshimeles099@gmail.com>
2023-08-25 14:21:42 +0300

    route

--- src/components/Switch/switch.js
+++ src/components/Switch/switch.js
@@ -1,35 +1,32 @@
-import { useState } from "react";
+import { useState, } from "react";
+import { useNavigate  } from "react-router-dom";
 import "./switch.css";
-import { Link } from "react-router-dom";
-//  function Switch(){
 
-//     return (
-//       <lable className="switch">
-//         <input type="checkbox" />
-//         <span className="slider">
-//         </span>
-//       </lable>
-//     )
-// }
-// export default Switch;
 function Switchs() {
+  const navigate = useNavigate();
   const [isOn, setIsOn] = useState(false);
 
   const handleToggle = () => {
     setIsOn(!isOn);
+    if (window.location.pathname === "/") {
+      navigate("/code");
+    } else {
+      navigate("/");
+    }
+   
   };
+
+
 
   return (
     <div className="div2">
       <label className="switch">
         <input type="checkbox" checked={isOn} onChange={handleToggle} />
         <span className="slider">
-          <Link to="/code">
             <div className="back">
               <span className="chat">chat mode</span>
               <span className="code">code mode</span>
             </div>
-          </Link>
         </span>
       </label>
     </div>
